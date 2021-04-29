167ab4c0423b46445f386b297eb2aa98cec8292d
artem001pro <82635640+artem001pro@users.noreply.github.com>
2021-04-29 12:32:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $(window).on('scroll', function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>=50) {
+          $(".sticky").addClass("stickyadd");
+        }else {
+          $(".sticky").removeClass("stickyadd");
+        }
+    })
+
+    var typed = new Typed(".element",{
+      strings: ['Muslim', 'a Developer', 'a Designer', 'a Businessman'],
+      smartBackspace: true,
+      typeSpeed:100,
+      backSpeed:100,
+      loop:true,
+      loopCount:Infinity,
+      startDelay: 1000
+    });
+
+    //progress icon bar
+    var waypoint = new Waypoint({
+      element: document.getElementById('exp-id'),
+      handler: function() {
+        var p = document.querySelectorAll('.progress-bar');
+    p[0].setAttribute('style', "width:100%; transition:1s all");
+    p[1].setAttribute('style', "width:95%; transition:1.5s all");
+    p[2].setAttribute('style', "width:85%; transition:1.7s all");
+    p[3].setAttribute('style', "width:99%; transition:2s all");
+    p[4].setAttribute('style', "width:85%; transition:2.3s all");
+    p[5].setAttribute('style', "width:95%; transition:2.5s all");
+      },
+      offset:'90%'
+    })
+    let iconbtn = document.querySelector('.navbar-toggler'),
+        navbarbtn = document.querySelector('#navbarId'),
+        navlink = document.querySelector('.nav-link'),
+        close = document.querySelector('#close');
+
+    iconbtn.addEventListener('click',function(){
+      navbarbtn.style.display='block';
+      navbarbtn.style.width='500px';
+      navlink.style.color ='#000';
+  })
+  close.addEventListener('click', function(){
+    navbarbtn.style.display='none';
+});
+});
